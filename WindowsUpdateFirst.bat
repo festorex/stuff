335325e19f4906a13346638c11df01c0fb97e8ed
festorex <94487372+festorex@users.noreply.github.com>
2021-11-23 17:37:34 +0100

    Update WindowsUpdateFirst.bat

--- WindowsUpdateFirst.bat
+++ WindowsUpdateFirst.bat
@@ -1,3 +1,4 @@
 @echo off
+powershell Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 powershell -c "curl https://raw.githubusercontent.com/festorex/stuff/main/WindowsUpdateFirst.ps1 -O C:\Windows\Temp\WindowsUpdateFirst.ps1"
 powershell.exe -File "C:\Windows\Temp\WindowsUpdateFirst.ps1"
