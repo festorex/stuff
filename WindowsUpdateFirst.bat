cb1d47e8a0cd8556463adb0c334156655cddc7e6
festorex <94487372+festorex@users.noreply.github.com>
2021-11-23 17:23:33 +0100

    Update WindowsUpdateFirst.bat

--- WindowsUpdateFirst.bat
+++ WindowsUpdateFirst.bat
@@ -1,4 +1,3 @@
 @echo off
 powershell -c "curl https://raw.githubusercontent.com/festorex/stuff/main/WindowsUpdateFirst.ps1 -O C:\Windows\Temp\WindowsUpdateFirst.ps1"
 powershell.exe -File "C:\Windows\Temp\WindowsUpdateFirst.ps1"
-
