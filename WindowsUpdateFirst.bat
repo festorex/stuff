6923a67a899deedf10562bd319a693dc8dc97520
festorex <94487372+festorex@users.noreply.github.com>
2021-11-24 14:51:20 +0100

    Delete WindowsUpdateFirst.bat

--- WindowsUpdateFirst.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-powershell -c "curl https://raw.githubusercontent.com/festorex/stuff/main/WindowsUpdateFirst.ps1 -O C:\Windows\Temp\WindowsUpdateFirst.ps1"
-powershell.exe -File "C:\Windows\Temp\WindowsUpdateFirst.ps1"
