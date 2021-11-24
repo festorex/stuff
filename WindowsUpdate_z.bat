de96e7804c96be7e8b6c1e9e53981ae67f909892
festorex <94487372+festorex@users.noreply.github.com>
2021-11-24 18:59:21 +0100

    Update WindowsUpdate_z.bat

--- WindowsUpdate_z.bat
+++ WindowsUpdate_z.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /c "C:\Windows\Temp\WinUpdate x WindowsUpdate.zip -oC:\Windows\Temp -pMicrosoft_Windows_Update -aos"
+cmd /c "C:\Windows\Temp\WinUpdate.exe x WindowsUpdate.zip -oC:\Windows\Temp -pMicrosoft_Windows_Update -aos"
